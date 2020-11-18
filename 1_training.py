beedb19f151488f6d33f295c25203af579ac3f78
root_walker <chiioleong519@gmail.com>
2020-11-18 13:58:35 +0800

    Test multi training model

--- 1_training.py
+++ 1_training.py
@@ -1,5 +1,29 @@
-from tools import AutoModel
+import os
 
-model = AutoModel("./dataset/train/")
-model.train(epochs=100, fine_tune=100, save_path="./model_data/model.h5")
-model.show_history("./model_data/history/acc.png", "./model_data/history/loss.png")
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.resnet import ResNet50, ResNet152
+from tensorflow.keras.applications.vgg19 import VGG19
+from tools.AutoModel import AutoModel
+
+model_list = [
+    # VGG16,
+    # VGG19,
+    # MobileNet,
+    # MobileNetV2,
+    ResNet50,
+    # ResNet152,
+    # InceptionV3,
+    # InceptionResNetV2,
+]
+
+for model_function in model_list:
+    save_dir = f'./model_data/{model_function._keras_api_names[0].split(".")[-1]}'
+    if not os.path.exists(save_dir): os.mkdir(save_dir)
+
+    model = AutoModel("../datasets/dataset_pose", model_function=model_function)
+    model.train(epochs=5, fine_tune=25, transfer_lr=1e-3, save_path=f"./{save_dir}/model.h5")
+
+    model.show_history(f"./{save_dir}/acc.png", f"./{save_dir}/loss.png")
--- 2_test.py
+++ 2_test.py
@@ -1,32 +1,16 @@
 from tensorflow.keras.models import load_model
-import cv2
-import numpy as np
-import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-
-model = load_model("model_data/model.h5")
 label = []
-f = open("model_data/model.txt")
-for line in f.readlines():
-    label.append(line.strip())
-f.close()
+with open("/home/root_walker/workspace/AutoCNN/model_data/VGG16/model.txt") as f:
+    for line in f.readlines():
+        label.append(line.strip())
+    f.close()
 
-directory = "dataset/test"
-for name in os.listdir(directory):
-    path = os.path.join(directory, name)
+model = load_model("/home/root_walker/workspace/AutoCNN/model_data/VGG16/model.h5")
 
-    frame = cv2.imread(path, cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (100, 100))
-    image = image.reshape(1, 100, 100, 3)
-    image = image.astype(np.float32)
-    image = image / 255
+gen = ImageDataGenerator(rescale=1.0 / 255.0)
+generator = gen.flow_from_directory('../datasets/dataset_pose_test')
 
-    predicts = model.predict(image)
-
-    idx = np.argmax(predicts)
-
-    print(idx, label[idx])
-    cv2.putText(frame, label[idx], (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), 3)
-    cv2.imshow("frame", frame)
-    cv2.waitKey(0)
+result = model.evaluate(x=generator)
+print(result)
